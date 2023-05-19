9fa8dd39af6a64f07df703f0d5300a0cb124fbd7
pkpkk <126570591+pkpkk@users.noreply.github.com>
2023-05-19 23:51:20 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from threading import Thread
+from flask import send_from_directory,Flask
+import os
+from subprocess import Popen
+
+FILE_PATH=os.path.join(os.getcwd(),'files/')
+PORT=os.getenv('PORT')
+HOST=os.getenv('HOST') if os.getenv('HOST') else 'https://0.0.0.0'
+os.mkdir(FILE_PATH)
+web_app=Flask(__name__)
+
+@web_app.route('/')
+def list_f():
+  html=""
+  for file in os.listdir(FILE_PATH):
+    html+=f'<a href="{HOST}/{file}"> {file} </a>'
+  return '<p>'+html+'</p>'
+
+@web_app.route('/<name>')
+def sendf(name):
+  return send_from_directory(FILE_PATH,name)
+
+
+if __name__=='__main__':
+  Popen(f'gunicorn 
